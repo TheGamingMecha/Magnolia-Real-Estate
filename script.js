1ba8bc0082e75576108129bd9f58a9f600d57488
Cain Arnold <69591442+Cain-Arnold@users.noreply.github.com>
2020-12-14 08:16:06 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'bs-11',
+          componentName:'PopupModal',
+          props:props,
+          context:context,
+          contextKey:'context-bs-1',
+          radpack:false
+        });
+      })({"upgradeable":false,"preset":"popup1","id":"d75b83f2-1126-451e-aeeb-1961df15a983","alignmentOption":"center","websiteId":"24c8dfba-986c-445e-bb1b-5ccc91952be1","lastPublished":"2020-11-11T16:03:20.673Z","enabled":true,"popupTitle":"Announcement","popupDescription":"Welcome! Check out my new announcement.","cta":{"enabled":true,"label":"Learn more","pageId":"8533cb96-f20e-4bb4-b0af-3e133a199be0"},"popupImage":{"image":""},"widgetId":"d75b83f2-1126-451e-aeeb-1961df15a983","section":"default","category":"neutral","locale":"en-US","renderMode":"PUBLISH"},{"widgetId":"d75b83f2-1126-451e-aeeb-1961df15a983","widgetType":"POPUP","widgetPreset":"popup1","section":"default","category":"neutral","fontSize":"medium","fontFamily":"alternate","websiteThemeOverrides":{},"widgetThemeOverrides":{}});
